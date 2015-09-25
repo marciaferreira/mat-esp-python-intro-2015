501a298884a1139d6a30c7981fe34334996d674e
marciaferreira <marciaferreira@yahoo.com>
2015-09-25 10:56:52 -0300

    criou graficos para cada iteracao independente de troca

--- luiza-marcia-gabriel/fig/bubble-sort.py
+++ luiza-marcia-gabriel/fig/bubble-sort.py
@@ -6,21 +6,30 @@ carta = [11, 18, 3, 1, 16, 12, 6, 19, 5, 0, 14, 4, 17, 9, 13, 7, 10, 15, 2, 8]
 print("lista original:", carta)
 # atribuimos a variavel n os valores da variavel carta
 n = 20
-x = 0
+troca = 0
+iteracao = 0
 plt.figure()
 plt.plot(range(0,n), carta, 'ok')
 plt.savefig("bubble-inicio.png")
+plt.close()
 # atribuimos ao indice i, o intervalo que varia de 0 a 18
 for i in range (0, n-1):
 # atribuimos ao indice j, o intervalo que varia entre i+1 a 19
     for j in range (i+1, n):
+        iteracao = iteracao + 1
+        plt.figure()
+        plt.plot(range(0,n), carta, 'ok')
+        plt.plot(i, carta[i], 'or')
+        plt.plot(j, carta[j], 'ob')
+        plt.savefig("bubble-it{}.png".format(iteracao))
+        plt.close()
 # estabelecemos uma comparação: se carta i eh menor que carta j
         if carta[i]<carta[j]:
 # se a comparacao for verdadeira entao mantem
             continue
 # senao
         else:
-            x = x+1
+            troca = troca+1
 # criamos um arquivo temporario que recebe o valor da carta i
             temp=carta[i]
 # a carta i recebe o valor da carta j
@@ -29,8 +38,8 @@ for i in range (0, n-1):
             carta[j]=temp
             plt.figure()
             plt.plot(range(0,n), carta, 'ok')
-            plt.savefig("bubble-troca{}.png".format(x)
-            
+            plt.savefig("bubble-troca{}.png".format(troca))
+            plt.close()
 # imprime o texto lista em ordem crescente com os valores ordenados
 print("lista em ordem crescente:", carta)
 # imprime os cinco menores valores da lista
@@ -41,4 +50,5 @@ print("cinco maiores valores:", carta[n-1:n-6:-1])
 plt.figure()
 plt.plot(range(0,n), carta, 'ok')
 plt.savefig("bubble-fim.png")
+plt.close()
 
